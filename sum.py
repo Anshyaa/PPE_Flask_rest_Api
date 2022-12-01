00fb93f15f90fbc3a33af7f2bdd8974fb1135d30
Anshyaa <110979027+Anshyaa@users.noreply.github.com>
2022-12-02 00:04:10 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,10 +1,93 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask.json import JSONEncoder
+from bson import json_util
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import JWTManager
+from blocklist import BLOCKLIST
 import mongo
-
-from flask import request, Flask
-
 app = Flask(__name__)
 
-@app.route("/camera/id", methods=["POST"])
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+user=[]
+password=[]
+username_password={"vinayak":"1234",
+                    "anshuman":"5678" }
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    
+   
+    if username in user :
+        return jsonify({"msg": "user alredy exit"}), 401
+    else:
+        # item={"username":username,"password":password}
+         user.append(username)
+         username_password[username]=password
+       # user.append(password)
+         return jsonify({'msg':"user succsesfully registered"})
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/users", methods=["GET"])
+@jwt_required()
+def get_all_users():
+    # Access the identity of the current user with get_jwt_identity
+    #current_user = get_jwt_identity()
+    return jsonify({ "user":user}), 200
+
+#user login method
+
+@app.route("/login",methods=["POST"])
+def login():
+    useName=request.json.get("username",None)
+    pword=request.json.get("password",None)
+    if useName in user and username_password[useName]==pword:
+        acc_token=create_access_token(identity=useName)
+        return jsonify(acc_token=acc_token)
+   
+    
+    return jsonify({"msg":"wrong username or password"}),401
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blocklist(jwt_header, jwt_payload):
+    return jwt_payload["jti"] in BLOCKLIST
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return (
+        jsonify(
+            {"description": "The token has been revoked.", "error": "token_revoked"}
+        ),
+        401,
+    )
+
+@app.route("/logout", methods=["POST"])
+
+@jwt_required()
+def logout():
+    jti = get_jwt()["jti"]
+    BLOCKLIST.add(jti)
+    return {"message": "Successfully logged out"}, 200
+
+
+@app.route("/camera", methods=["POST"])
 def addcamera():
     camera = request.json
     if camera is None:
@@ -22,29 +105,22 @@ def addalert():
     return {"message":"msg successfully added"}
 
 
-@app.route("/alert/msg_id", methods=["POST"])
-def getalert():
-    msg_id = request.json
-    if msg_id is None:
-        return {"message":"No msg_id found"}
-    mongo.get_alert(msg_id)
-    return {"messager": "msg_id successfully added"}
-    
+class CustomJSONEncoder(JSONEncoder): 
+    def default(self, obj):
+        return json_util.default(obj)
 
-@app.route("/camera", methods=["POST"])
-def getcamera():
-    cam_id = request.json
-    if cam_id is None:
-        return {"message":"No cam_id found"}
-    mongo.get_alert(cam_id)
-    return {"messager": "cam_id successfully added"}
-
-
+y = CustomJSONEncoder()
 @app.route("/cameras",methods=["GET"])
 def cameras():
-    return mongo.client["my_db"]["camera"]
-    
- 
+    all_cameras = mongo.all_camera()
+    if all_cameras is None:
+        return {"message": "No Camera found"}
+    camera_list = []
+    for x in all_cameras:
+        camera_list.append(x)
+    return jsonify(camera_list)
+
+        
 @app.route("/camera/id",methods=["GET"])
 def camera_id():
     #geting cam_id
@@ -54,14 +130,34 @@ def camera_id():
 
 @app.route("/alerts",methods=["GET"])
 def alerts():
-    mongo.all_alert()   
+    mongo.all_alert() 
+    all_alerts = mongo.all_camera()
+    if all_alerts is None:
+        return {"message": "No alert found"}
+    alert_list = []
+    for x in all_alerts:
+        alert_list.append(x)
+    return jsonify(alert_list)  
     
 @app.route("/alert/msg_id",methods=["GET"])
 def alert_msg():
-    al=request.json.get("msg_id",None)
-    mongo.get_alert(al)
+    alrt=request.json.get("msg_id",None)
+    return mongo.get_alert(alrt)
+
+
+@app.route("/page", methods= ["GET"])
+def get_page():
+    page=request.json.get("page_no",None)
+    entry=request.json.get("entry_no",None)
+    pegina=mongo.pagination(entry,page)
+    data=[]
+    for x in pegina:
+        #x["_id"]=str(x["_id"])
+        data.append(x)
+    return jsonify(data)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
